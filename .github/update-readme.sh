ef7625bd4291943dca31e2160460921683ca0eeb
Ben Roberts <ben@headsnet.com>
2022-01-31 16:14:51 +0100

    Update update-readme.sh

--- .github/update-readme.sh
+++ .github/update-readme.sh
@@ -5,16 +5,10 @@ echo -e "=====\n"
 
 echo -n "A collection of small tips and tricks that I pick up each day I work in tech."
 
-
 for directory in */ ; do
-
     echo -e "\n\n### ${directory%/}\n\n"
-
-  	for file in $directory* ; do
-
-		read title < $file
-
+    for file in $directory* ; do
+        read title < $file
         echo -e "- [$title]($file)"
-	done
-
+    done
 done

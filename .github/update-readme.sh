aeede2cff7bb44611facd486308969029f597dfc
Ben Roberts <ben@headsnet.com>
2022-01-31 16:39:02 +0100

    Capitalise section titles

--- .github/update-readme.sh
+++ .github/update-readme.sh
@@ -6,8 +6,10 @@ echo -e "=====\n"
 echo -n "A collection of small tips and tricks that I pick up each day I work in tech."
 
 for directory in */ ; do
-    echo -e "\n\n### ${directory%/}\n\n"
-    for file in $directory* ; do
+    directory=${directory%/}
+    directoryName=${directory^^}
+    echo -e "\n\n### $directoryName\n\n"
+    for file in $directory/* ; do
         read title < $file
         echo -e "- [$title]($file)"
     done

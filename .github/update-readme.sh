c6e221c3fe7a6a038b103df0cdce5ecbcc0d0f87
Ben Roberts <ben@headsnet.com>
2022-01-31 16:10:38 +0100

    Add basic publishing system

---
+++ .github/update-readme.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Today I Learned"
+echo -e "=====\n"
+
+echo -n "A collection of small tips and tricks that I pick up each day I work in tech."
+
+
+for directory in */ ; do
+
+    echo -e "\n\n### ${directory%/}\n\n"
+
+  	for file in $directory* ; do
+
+		read title < $file
+
+        echo -e "- [$title]($file)"
+	done
+
+done
